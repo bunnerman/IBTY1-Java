20ae40efd9903eababbb55ecd46652c2cd5fc9d6
bunnerman <parnilrbalawade@gmail.com>
2026-07-29 22:22:17 +0530

    Update 03F Length of String (Even or Odd).java

--- 03F Length of String (Even or Odd).java
+++ 03F Length of String (Even or Odd).java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
 
+class Example
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Enter string: ");
+		String st;
+		Scanner sc = new Scanner(System.in);
+		st = sc.nextLine();
+		String r;
+		if (st.length() % 2 == 1)
+			r = "Odd";
+		else
+			r = "Even";
+		System.out.println("Length of given string is " + r);
+	}	
+} 
