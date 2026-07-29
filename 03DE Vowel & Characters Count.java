7372be25cea6a90ad53ef02e087fce7b2cb3b1a4
bunnerman <parnilrbalawade@gmail.com>
2026-07-29 21:19:54 +0530

    Update and rename 03D Vowel Count.java to 03DE Vowel & Characters Count.java

--- 03DE Vowel & Characters Count.java
+++ 03DE Vowel & Characters Count.java
@@ -10,12 +10,15 @@ class Example
 		st = sc.nextLine();
 		int len = st.length();
 		int vowelCount = 0;
+		int charCount = 0;
 		for (int i = 0; i < len; i++)
 		{
 			char ch = st.charAt(i);
+			charCount++;
 			if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
 				vowelCount++;
 		}
 		System.out.println(vowelCount + " vowels are present");
+		System.out.println(charCount + " characters are present");
 	}	
 } 
