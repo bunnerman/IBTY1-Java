205138e52ddb39232dbe7716cf6c40f9d521bd4c
bunnerman <parnilrbalawade@gmail.com>
2026-07-30 10:37:50 +0530

    Update 04B Wrapper String to Int Sum.java

--- 04B Wrapper String to Int Sum.java
+++ 04B Wrapper String to Int Sum.java
@@ -4,11 +4,10 @@ public class Example
 {
     public static void main(String[] args) 
 	{
-		System.out.print("Enter integer X: ");
+		System.out.print("Enter 2 integers X Y: ");
 		Scanner sc = new Scanner(System.in);
-		String x = sc.nextLine();
-		System.out.print("Enter integer Y: ");
-		String y = sc.nextLine();
+		String x = sc.next();
+		String y = sc.next();
 
 		Integer wrprObj =  Integer.valueOf(x);
 		int m = wrprObj.intValue();
